0e9bea5201909b7003897f1c9a211d4f54e0065d
ayeshamansuri <141137316+ayeshamansuri@users.noreply.github.com>
2023-09-25 11:52:44 +0530

    Update grubhub_parser.py

--- grubhub_parser.py
+++ grubhub_parser.py
@@ -5,8 +5,6 @@ with open("grubhub_menu.json", "r") as menu:
     
 grubhub_menu = data
 menu = dict()
-
-
 # modifiers
 menu["modifiers"] = {}
 for modifier in grubhub_menu["modifiers"]:
@@ -19,30 +17,29 @@ for modifier in grubhub_menu["modifiers"]:
     new_modifier["image_url"] = None
     menu["modifiers"][modifier["uuid"]] = new_modifier
 
-
 menu["modifier_lists"] = {}
 for modifier_list in grubhub_menu["modifier_lists"]:
     menu["modifier_lists"][modifier_list["uuid"]] = modifier_list
 
-#size_prompts
+# size_prompts
 menu["size_prompts"] = {}
 for size_prompt in grubhub_menu["size_prompts"]:
     menu["size_prompts"][size_prompt["uuid"]] = size_prompt
 
-#modifiers_categories
+# modifiers_categories
 menu["modifier_categories"] = {}
 for modifier_category in grubhub_menu["modifier_prompts"]:
-    new_modifier_category = {}
-    new_modifier_category["title"] =  modifier_category["latest_version"]["name"]
+    new_modifier_category = dict()
+    new_modifier_category["title"] = modifier_category["latest_version"]["name"]
     new_modifier_category["image_url"] = None
     new_modifier_category["price"] = None
     new_modifier_category["description"] = None
     new_modifier_category["modifiers"] = []
-    for modifier_uuid in menu["modifier_lists"][modifier_category["latest_version"]["modifier_list"]]["latest_version"]["modifiers"]:
+    for modifier_uuid in \
+            menu["modifier_lists"][modifier_category["latest_version"]["modifier_list"]]["latest_version"][
+                "modifiers"]:
         new_modifier_category["modifiers"].append(menu["modifiers"][modifier_uuid])
     menu["modifier_categories"][modifier_category["uuid"]] = new_modifier_category
-
-
 
 # items
 items = {}
@@ -59,30 +56,26 @@ for item in grubhub_menu["items"]:
     new_item["modifier_categories"] = []
     for modifier_category_uuid in item["modifier_prompts"]:
         new_item["modifier_categories"].append(menu["modifier_categories"][modifier_category_uuid])
-#add modifer_category in itme for size vice price.
+    # add modifier_category in itme for size vice price.
     if "size_prompt" in item:
-        new_modifer_category = {}
-        new_modifer_category["title"] = menu["size_prompts"][item["size_prompt"]]["latest_version"]["name"]
-        new_modifer_category["description"] = None
-        new_modifer_category["price"] = None
-        new_modifer_category["image_url"] = None
-        new_modifer_category["modifiers"] = []
+        new_modifier_category = dict()
+        new_modifier_category["title"] = menu["size_prompts"][item["size_prompt"]]["latest_version"]["name"]
+        new_modifier_category["description"] = None
+        new_modifier_category["price"] = None
+        new_modifier_category["image_url"] = None
+        new_modifier_category["modifiers"] = []
         for size_data in menu["size_prompts"][item["size_prompt"]]["latest_version"]["sized_prices"]:
-            new_modifier = {}
+            new_modifier = dict()
             new_modifier["name"] = size_data["display_name"]
             new_modifier["price"] = size_data["price"]
             new_modifier["description"] = None
             new_modifier["image_url"] = None
-            new_modifer_category["modifiers"].append(new_modifier)
+            new_modifier_category["modifiers"].append(new_modifier)
             new_modifier["sub_modifier_categories"] = []
 
-        new_item["modifier_categories"].append(new_modifer_category)
-
+        new_item["modifier_categories"].append(new_modifier_category)
     items[item["uuid"]] = new_item
-
 menu["items"] = items
-
-
 
 # categories
 categories = {}
@@ -101,11 +94,9 @@ for category in grubhub_menu["menu_sections"]:
     categories[category["uuid"]] = new_category
 menu["categories"] = categories
 
-
-
 # menu
 menu["menu"] = {
-    "title": "title_grubhub",
+    "title": "title_grubhub1",
     "type": None,
 }
 menu["menu"]["categories"] = []
