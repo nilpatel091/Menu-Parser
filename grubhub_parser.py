7dd5a07157c0e042cf95a58e7800c527717ef063
Nil patel <nil.patel@scriptalldna.com>
2023-09-25 10:55:28 +0530

    change in grubhub_parser

--- grubhub_parser.py
+++ grubhub_parser.py
@@ -5,24 +5,44 @@ with open("grubhub_menu.json", "r") as menu:
     
 grubhub_menu = data
 menu = dict()
+
+
 # modifiers
 menu["modifiers"] = {}
 for modifier in grubhub_menu["modifiers"]:
     modifier = modifier["latest_version"]
     new_modifier = dict()
-    new_modifier["title"] = modifier["name"]
+    new_modifier["name"] = modifier["name"]
     new_modifier["description"] = modifier["description"]
     new_modifier["price"] = modifier["default_price"]
+    new_modifier["sub_modifier_categories"] = []
     new_modifier["image_url"] = None
     menu["modifiers"][modifier["uuid"]] = new_modifier
+
 
 menu["modifier_lists"] = {}
 for modifier_list in grubhub_menu["modifier_lists"]:
     menu["modifier_lists"][modifier_list["uuid"]] = modifier_list
 
-menu["modifier_prompts"] = {}
-for modifier_prompt in grubhub_menu["modifier_prompts"]:
-    menu["modifier_prompts"][modifier_prompt["uuid"]] = modifier_prompt
+#size_prompts
+menu["size_prompts"] = {}
+for size_prompt in grubhub_menu["size_prompts"]:
+    menu["size_prompts"][size_prompt["uuid"]] = size_prompt
+
+#modifiers_categories
+menu["modifier_categories"] = {}
+for modifier_category in grubhub_menu["modifier_prompts"]:
+    new_modifier_category = {}
+    new_modifier_category["title"] =  modifier_category["latest_version"]["name"]
+    new_modifier_category["image_url"] = None
+    new_modifier_category["price"] = None
+    new_modifier_category["description"] = None
+    new_modifier_category["modifiers"] = []
+    for modifier_uuid in menu["modifier_lists"][modifier_category["latest_version"]["modifier_list"]]["latest_version"]["modifiers"]:
+        new_modifier_category["modifiers"].append(menu["modifiers"][modifier_uuid])
+    menu["modifier_categories"][modifier_category["uuid"]] = new_modifier_category
+
+
 
 # items
 items = {}
@@ -36,17 +56,31 @@ for item in grubhub_menu["items"]:
         new_item["price"] = item["price_variations"]["minimum_price"]
     new_item["description"] = item["description"]
     new_item["image_url"] = None
-    new_item["modifiers"] = []
-    for modifier_prompt_uuid in item["modifier_prompts"]:
-        modifier_list_uuid = menu["modifier_prompts"][modifier_prompt_uuid][
-            "latest_version"
-        ]["modifier_list"]
-        for modifier_uuid in menu["modifier_lists"][modifier_list_uuid][
-            "latest_version"
-        ]["modifiers"]:
-            new_item["modifiers"].append(menu["modifiers"][modifier_uuid])
+    new_item["modifier_categories"] = []
+    for modifier_category_uuid in item["modifier_prompts"]:
+        new_item["modifier_categories"].append(menu["modifier_categories"][modifier_category_uuid])
+#add modifer_category in itme for size vice price.
+    if "size_prompt" in item:
+        new_modifer_category = {}
+        new_modifer_category["title"] = menu["size_prompts"][item["size_prompt"]]["latest_version"]["name"]
+        new_modifer_category["description"] = None
+        new_modifer_category["price"] = None
+        new_modifer_category["image_url"] = None
+        new_modifer_category["modifiers"] = []
+        for size_data in menu["size_prompts"][item["size_prompt"]]["latest_version"]["sized_prices"]:
+            new_modifier = {}
+            new_modifier["name"] = size_data["display_name"]
+            new_modifier["price"] = size_data["price"]
+            new_modifier["description"] = None
+            new_modifier["image_url"] = None
+            new_modifer_category["modifiers"].append(new_modifier)
+        new_item["modifier_categories"].append(new_modifer_category)
+
     items[item["uuid"]] = new_item
+
 menu["items"] = items
+
+
 
 # categories
 categories = {}
@@ -65,6 +99,8 @@ for category in grubhub_menu["menu_sections"]:
     categories[category["uuid"]] = new_category
 menu["categories"] = categories
 
+
+
 # menu
 menu["menu"] = {
     "title": "title_grubhub",
@@ -78,12 +114,11 @@ for section in grubhub_menu["menu_info"]["latest_version"]["sections"]:
 del menu["items"]
 del menu["categories"]
 del menu["modifiers"]
-del menu["modifier_prompts"]
+del menu["modifier_categories"]
 del menu["modifier_lists"]
-return menu["menu"]
+del menu["size_prompts"]
+
 with open("grubhub_formatted_menu.json","w") as file:
-    json.dump(menu, file, indent=4)
-
-
+    json.dump(menu["menu"], file, indent=4)
 
 
