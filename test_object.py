92e3d7e938fa5335989c37521e87d8497f529fbe
Nil patel <nil.patel@scriptalldna.com>
2023-09-28 12:16:21 +0530

    add test object

---
+++ test_object.py
@@ -0,0 +1,51 @@
+{
+    "title": "Menu",
+    "type": null,
+    "categories": [  
+        {
+            "title": "Cold Subs",
+            "subtitle": null,
+            "description": null,
+            "items":  
+            {
+                    "title": "Cold Tuna Sub",
+                    "description": "",
+                    "image_url": null,
+                    "is_archive": false,
+                    "price": 1275,
+                    "modifier_categories": [
+                        {
+                            "title": "Choose your topping",
+                            "price": null,
+                            "image_url": null,
+                            "description": null,
+                            "modifiers": [ 
+                                {
+                                    "name": "Topping",
+                                    "description": "",
+                                    "image_url": null,
+                                    "price": 0,
+                                    "sub_modifier_categories": [
+                                        {
+                                            "title": "Choose your preparation",
+                                            "price": null,
+                                            "image_url": null,
+                                            "description": null,
+                                            "sub_modifiers": [
+                                                {
+                                                    "name": "Cheese",
+                                                    "description": null,
+                                                    "image_url": null,
+                                                    "price": 100
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+        }
+    ]
+}
